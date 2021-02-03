e6913411206a4cae00f1f6791a9698e3e0e17ea4
Kaung Shein <nicholasling1412@gmail.com>
2021-02-03 18:46:27 -0500

    set up server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const logger = require("morgan");
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+
+////// MIDDLEWARE //////
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: 200,
+    message: "Tuna Tunr Test Trial",
+  });
+});
+
+////// ROUTES FOR CONTROLLERS //////
+
+app.listen(PORT, () => console.log(`listening in on port: ${PORT}`));
